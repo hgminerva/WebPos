a60abb8e497d809cae0884fb56e869167c5dd08f
justcallm3ian <justcallm3aaron@gmail.com>
2015-11-27 14:37:40 +0800

    added mstItemInventory to MstTable Model

---
+++ ISWebPOS/Models/MstItemInventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISWebPOS.Models
+{
+    public class MstItemInventory
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public Int32 ItemId { get; set; }
+        public DateTime InventoryDate { get; set; }
+        public Double Quantity { get; set; }
+    }
+}
---
+++ ISWebPOS/Models/MstItemPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISWebPOS.Models
+{
+    public class MstItemPrice
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public Int32 ItemId { get; set; }
+        public String PriceDescription { get; set; }
+        public Double Price { get; set; }
+        public Double TriggerQuantity { get; set; }
+    }
+}
