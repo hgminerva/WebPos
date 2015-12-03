581254a0bc449bbe51440e2415b1e9232c7dd30a
resxustrya <rexustraya@gmail.com>
2015-12-03 07:48:49 -0800

    Added TrnStockCountLine to TrnStockOutline models

---
+++ ISWebPOS/Models/TrnStockIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISWebPOS.Models
+{
+    public class TrnStockIn
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public Int32 PeriodId { get; set; }
+        public DateTime StockInDate { get; set; }
+        public String StockInNumber { get; set; }
+        public Int32 SupplierId { get; set; }
+        public String Remarks { get; set; }
+        public Boolean IsReturn { get; set; }
+        public Int32 CollectionId { get; set; }
+        public Int32 PurchaseOrderId { get; set; }
+        public Int32 Prepareby { get; set; }
+        public Int32 CheckedBy { get; set; }
+        public Int32 ApprovedBy { get; set; }
+        public Boolean IsLocked { get; set; }
+        public Int32 EntryUserId { get; set; }
+        public String EntryDateTime { get; set; }
+        public Int32 UpdateUserId { get; set; }
+        public String UpdateDateTime { get; set; }
+    }
+}
